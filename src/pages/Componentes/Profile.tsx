c2179c60b05be1819bd7798e264f6080b32f51dc
marcosvictor0 <marcos_victor159@hotmail.com>
2021-04-25 12:20:44 -0300

    feito o profile e o CompletedChallenges

---
+++ src/pages/Componentes/Profile.tsx
@@ -0,0 +1,17 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile () {
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/marcosvictor0.png" alt="Marcos Victor"/>
+            <div>
+                <strong>Marcos Victor</strong>
+
+                <p>
+                    <img src="icons/level.svg" alt=""/>
+                    Level 1
+                </p>
+            </div>
+        </div>
+    );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,25 @@
+import { CompletedChallenges } from './Componentes/CompletedChallenges';
 import Head from 'next/head';
 import { ExperienceBar } from './Componentes/ExperienceBar';
+import { Profile } from './Componentes/Profile';
+import styles from './styles/pages/Home.module.css';
+
+
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={ styles.container}>
       <ExperienceBar/>
+
+      <section>
+        <div>
+          <Profile/>
+          <CompletedChallenges/>
+        </div>
+        <div>
+
+        </div>
+      </section>
     </div>
   )
 }
