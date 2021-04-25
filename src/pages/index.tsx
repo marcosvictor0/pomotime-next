9f0c40167690554d6d3c60eefb30b26a83ce7339
marcosvictor0 <marcos_victor159@hotmail.com>
2021-04-25 13:30:32 -0300

    att do css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { CompletedChallenges } from './Componentes/CompletedChallenges';
+import { CountDown } from './Componentes/CountDown';
 import Head from 'next/head';
 import { ExperienceBar } from './Componentes/ExperienceBar';
 import { Profile } from './Componentes/Profile';
@@ -9,12 +10,16 @@ import styles from './styles/pages/Home.module.css';
 export default function Home() {
   return (
     <div className={ styles.container}>
+      <Head>
+        <title>Inicio | PomoTime</title>
+      </Head>
       <ExperienceBar/>
 
       <section>
         <div>
           <Profile/>
           <CompletedChallenges/>
+          <CountDown/>
         </div>
         <div>
 
