7afafa9a8851a80effb27d42ca6e050e6f036947
marcosvictor0 <marcos_victor159@hotmail.com>
2021-05-01 15:21:53 -0300

    adc estilização no novo botão e feito +1 botão

--- src/pages/Componentes/CountDown.tsx
+++ src/pages/Componentes/CountDown.tsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from 'react';
 import styles from '../styles/components/CountDown.module.css';
 
+let countdownTimeOut: NodeJS.Timeout;
+
 export function CountDown() {
-    const [time, setTime] = useState(25 * 60);
+    const [time, setTime] = useState( 0.1 * 60 );
     const [isActive, setIsActive] = useState(false);
+    const [hasFinished, setHasFinished] = useState(false);
 
     const minutes = Math.floor(time / 60);
     const seconds = time % 60;
@@ -15,11 +18,21 @@ export function CountDown() {
         setIsActive(true);
     }
 
+
+    function resetCountDown() {
+        clearTimeout(countdownTimeOut);
+        setIsActive(false);
+        setTime( 0.1 * 60 );
+    }
+
     useEffect(() => {
         if (isActive && time > 0 ){
-            setTimeout(() => {
+            countdownTimeOut = setTimeout(() => {
                 setTime(time - 1);
             }, 1000)
+        } else if (isActive && time === 0) {
+            setHasFinished(true);
+            setIsActive(false);
         }
     }, [isActive, time])
     //essa função faz o contador la no site.
@@ -38,12 +51,23 @@ export function CountDown() {
             </div>
         </div>
 
+        { hasFinished ? (
+            <button
+            disabled
+            className={styles.countDownButton}
+            >
+               Ciclo encerrado
+            </button> 
+        ) : null //isso faz com qu eele tenha apenas um IF, a outra forma seria que no lugar do '?', colocaria um && e tirasse o : null.
+    }
+
+
         { isActive ? 
         (
             <button 
             type="button" 
             className={`${styles.countDownButton} ${styles.countDownButtonActive}`}
-            onClick={startCountDown}
+            onClick={resetCountDown}
             >
                Abandonar ciclo
             </button> 
